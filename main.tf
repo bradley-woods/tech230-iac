db3cf8349f2c187dfa413d6f68762a7ad518f443
Bradley Woods <BradleyWoods@spartaglobal.com>
2023-06-08 17:23:55 +0100

    Updated Terraform files

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@
 
 provider "aws" {
 # which region of AWS
-	region = "eu-west-1"
+	region = region
 	
 }
 # Git bash must have admin access
@@ -16,17 +16,17 @@ provider "aws" {
 resource "aws_instance" "app_instance" {
 
 # which AMI - ubuntu 18.04
-	ami = "ami-00e8ddf087865b27f"
+	ami = ami
 
 # type of instance - t2.micro
-	instance_type = "t2.micro"
+	instance_type = type
 
 # do you need public ip - yes
 	associate_public_ip_address = true
 
 # what would you like to call it
 	tags = {
-		Name = "tech230-bradley-terraform-app"
+		Name = name
 		
 	}
 }
